4d0ea2735a4b6f383454c1c5bc756b1af1a50184
Marvin Cayetano <cayetanomarvin@gmail.com>
2021-07-28 21:12:54 -0400

    🔥 Chore: Login

--- helpers/scrape.ts
+++ helpers/scrape.ts
@@ -1,11 +1,34 @@
-const puppeteer = require("puppeteer");
+import puppeteer from "puppeteer";
 
 async function scrapeForSched() {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto(<string>process.env.FIT4LESS_URL_LOGIN);
 
-  await browser.close();
+  const USERNAME: string = <string>process.env.FIT4LESS_USERNAME;
+  const PASSWORD: string = <string>process.env.FIT4LESS_PASSWORD;
+
+  // Goes to the main login page
+  await page.goto(<string>process.env.FIT4LESS_URL_LOGIN, {
+    waitUntil: "networkidle0",
+  });
+
+  await page.$eval(
+    "#emailaddress",
+    (el: Element, username: any) => ((<HTMLInputElement>el).value = username),
+    USERNAME
+  );
+
+  await page.$eval(
+    "#password",
+    (el: Element, password: any) => ((<HTMLInputElement>el).value = password),
+    PASSWORD
+  );
+
+  //   await page.$eval("#loginButton", (el: Element) =>
+  //     (<HTMLInputElement>el).click()
+  //   );
+
+  //   await browser.close();
 }
 
 export { scrapeForSched };
